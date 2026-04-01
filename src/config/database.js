ef20e75b828d01f5b660743d9955249df2ef53c1
wendziin <wdgamer1@gmail.com>
2026-04-01 16:03:49 -0300

    Add postgres support for Render

--- src/config/database.js
+++ src/config/database.js
@@ -16,10 +16,13 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
